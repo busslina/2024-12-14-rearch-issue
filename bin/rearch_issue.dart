1285317bd0bf1ec961bcd63e23411b0503078d63
Busslina <busslina@gmail.com>
2024-12-14 04:58:45 +0100

    Initial commit

---
+++ bin/rearch_issue.dart
@@ -0,0 +1,5 @@
+import 'package:rearch_issue/rearch_issue.dart' as rearch_issue;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rearch_issue.calculate()}!');
+}
---
+++ test/rearch_issue_test.dart
@@ -0,0 +1,8 @@
+import 'package:rearch_issue/rearch_issue.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
